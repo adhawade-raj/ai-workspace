9ba412ddcc35bdda28cb486df5b5e44e683d2f74
adhawade-raj <adhawaderaj@gmail.com>
2026-09-15 19:02:46 +0530

    Python fundamentals

---
+++ AI-ML-2026-PythonLectures/Part11_UserInput.py
@@ -0,0 +1,15 @@
+#Sum of 2 two with casting issue
+a = input("Value of a : ")
+b= input("Value of b : ")
+sum = a+b
+print(sum)
+
+#Sum of 2 two witout casting issue
+c = int(input("Value of c : "))
+d=  int(input("Value of d : "))
+sum = c+d
+
+print(sum)
+
+
+
---
+++ AI-ML-2026-PythonLectures/Part12_ConditionalStatemen.py
@@ -0,0 +1,26 @@
+age = 32
+if age>=18 :
+    print ("you can vote")
+else:
+    print("you cannot vote")
+
+
+
+print("---------------------elif-------------------")
+
+color = "black"
+
+if color == "green":
+    print("Go")
+elif color == "yellow":
+    print("Hold")
+elif color == "red":
+    print("Stop")
+else:
+    print("Invalid color")
+
+
+
+
+
+
