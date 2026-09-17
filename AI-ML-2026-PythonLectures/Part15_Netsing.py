c39c82e19331d0a0bd9fb4155379566429137626
adhawade-raj <adhawaderaj@gmail.com>
2026-09-17 18:53:47 +0530

    Add EvenOddNumber, Netsing, and MatchCase examples

---
+++ AI-ML-2026-PythonLectures/Part15_Netsing.py
@@ -0,0 +1,10 @@
+username = "Raj"
+password = "Pass"
+
+if (username =="admin" and password=="Pass"):
+    print("Success")
+else:
+    if(username != "admin"):
+        print("wrong username")
+    else:
+        print("wrong password")
---
+++ AI-ML-2026-PythonLectures/Part16_MatchCase.py
@@ -0,0 +1,11 @@
+color = "Red"
+
+match color :
+    case "Red":
+        print("The color is Red")
+    case "Blue":
+        print("The color is Blue")
+    case "Green":
+        print("The color is Green")
+    case _:
+        print("Unknown color")
